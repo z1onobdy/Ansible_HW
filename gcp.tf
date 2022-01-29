5f4d8206db403c4143517cce852ddce51e1138c2
z1onobody <z10nobody@gmail.com>
2022-01-30 01:24:17 +0200

    tst

--- gcp.tf
+++ gcp.tf
@@ -10,4 +10,13 @@ resource "google_compute_instance" "Node" {
   name         = "node${count.index}"
   hostname     = "node${count.index}.example.com"
   machine_type = "e2-medium"
+  boot_disk {
+    initialize_params {
+      image = "centos-8/centos-8-v20211214"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
 }
