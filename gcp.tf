43aeb541dd26d4ebf648d20654b51ac76c3eb6c8
z1onobody <z10nobody@gmail.com>
2022-01-30 01:06:13 +0200

    tst

--- gcp.tf
+++ gcp.tf
@@ -1,13 +1,13 @@
 provider "google" {
-credentials = file ()
-project = "HomeWork"
-region = "europe-central2"
-zone = "europe-central2-b"
+  credentials = file()
+  project     = "HomeWork"
+  region      = "europe-central2"
+  zone        = "europe-central2-b"
 }
 
 resource "google_compute_instance" "Node" {
-count = "2"
-name = "node${count.index}"
-hostname = "node${count.index}.example.com"
-machine_type = "e2-medium"
+  count        = "2"
+  name         = "node${count.index}"
+  hostname     = "node${count.index}.example.com"
+  machine_type = "e2-medium"
 }
