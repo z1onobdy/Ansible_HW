1e4dc05614b38ec8a6954ef6f3ea6119972f1953
z1onobody <z10nobody@gmail.com>
2022-01-30 11:25:04 +0200

    a

--- gcp.tf
+++ gcp.tf
@@ -17,7 +17,7 @@ resource "google_compute_instance" "Node" {
     }
   }
   metadata = {
-    ssh-keys = "root: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfI+vsEcyM5j+tDU5F+BAhAdNbJo4AaGVDe7TceNvjvFY8ZAbQEv1IX3cS9Nel+EWmM6gMmd1VHKtts1Y+YfnmOo3mg0pjY4YtOsz4/WtGw+1qKeeTrpuomVwuh3kIbl/aOuNsczY5AH7uoeXy030o6UFGv3xm5zfiSiSEuaEl0eI3R7980bYrUioFlIBkbBKttbYdSYFJpCg1xtPv8J1U0Jjbtn4vvJaVdpzChTI1MIrokseXItnFS8YkEzEiROZUJ0TyFyGRIYBW8crXElCJFJoBuECPSxnfvLuUTnZ7iTt7d8q05lsrONiHbyEnjFyQIZzmGJlSCwwPh7X7K2In z10"
+    ssh-keys = "z10: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfI+vsEcyM5j+tDU5F+BAhAdNbJo4AaGVDe7TceNvjvFY8ZAbQEv1IX3cS9Nel+EWmM6gMmd1VHKtts1Y+YfnmOo3mg0pjY4YtOsz4/WtGw+1qKeeTrpuomVwuh3kIbl/aOuNsczY5AH7uoeXy030o6UFGv3xm5zfiSiSEuaEl0eI3R7980bYrUioFlIBkbBKttbYdSYFJpCg1xtPv8J1U0Jjbtn4vvJaVdpzChTI1MIrokseXItnFS8YkEzEiROZUJ0TyFyGRIYBW8crXElCJFJoBuECPSxnfvLuUTnZ7iTt7d8q05lsrONiHbyEnjFyQIZzmGJlSCwwPh7X7K2In z10"
   }
 
   network_interface {
