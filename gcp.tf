0c368122cfb9ba04195c46fd4604b5499b205fb0
z1onobody <z10nobody@gmail.com>
2022-01-30 09:51:03 +0200

    tst

--- gcp.tf
+++ gcp.tf
@@ -1,18 +1,19 @@
 provider "google" {
-  credentials = file("hw.json")
-  project     = "HomeWork"
-  region      = "europe-central2"
-  zone        = "europe-central2-b"
+  project = "homework-339713"
+  region  = "europe-central2"
+  zone    = "europe-central2-a"
+
 }
 
 resource "google_compute_instance" "Node" {
-  count        = "2"
-  name         = "node${count.index}"
-  hostname     = "node${count.index}.example.com"
+  count    = "2"
+  name     = "node${count.index + 1}"
+  hostname = "node${count.index + 1}.example.com"
+
   machine_type = "e2-medium"
   boot_disk {
     initialize_params {
-      image = "centos-8/centos-8-v20211214"
+      image = "centos-cloud/centos-8-v20211214"
     }
   }
 
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "credentials" {
+  default = "~/terraform/Ansible_HW/hw.json"
+}
