1c8e9e098d797b3f9b557a00c39f7635614b35cf
z1onobdy <z10nobody@gmail.com>
2022-02-01 17:19:24 +0200

    cntrl repo fix

--- gcp.tf
+++ gcp.tf
@@ -53,7 +53,8 @@ resource "google_compute_instance" "cntrl" {
     }
 
     inline = [
-
+      "sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*",
+      "sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*",
       "sudo yum install epel-release -y",
       "sudo yum install ansible -y"
     ]
